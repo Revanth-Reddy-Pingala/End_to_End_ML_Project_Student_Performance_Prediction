d69f0773c6d349a1d6c7f9f5ba5d1bec5a04b08f
Revanth Reddy Pingala <pingalarevanthreddy144@gmail.com>
2024-01-29 14:02:07 +0530

    Prediction Pipeline

--- app.py
+++ app.py
@@ -9,13 +9,7 @@ application=Flask(__name__)
 
 app=application
 
-## Route for a home page
-
-@app.route('/')
-def index():
-    return render_template('index.html') 
-
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
